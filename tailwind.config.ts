875b18998f06b71ab059139bff1da101a09bc8d0
Yong Le He <ygle.he@gmail.com>
2024-12-01 18:39:34 -0500

    chore: update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,47 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 import colors from "tailwindcss/colors";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-      },
       colors: {
-        primary: colors.blue, // Primary Text, Active states and buttons
-        secondary: colors.fuchsia, // Secondary Text, Buttons
-        accent: colors.lime, // Highlighted information
-        base: colors.slate, // Primary backgrounds, borders, low constrast text
-
-        info: colors.sky,
-        success: colors.emerald,
-        warning: colors.amber,
-        error: colors.red,
+        primary: {
+          light: colors.blue["300"],
+          dark: colors.blue["600"],
+        },
+        secondary: {
+          light: colors.fuchsia["300"],
+          dark: colors.fuchsia["600"],
+        },
+        accent: {
+          light: colors.lime["400"],
+          dark: colors.lime["600"],
+        },
+        muted: {
+          light: colors.slate["200"],
+          dark: colors.slate["800"],
+        },
+        base: {
+          light: colors.slate["50"],
+          dark: colors.slate["700"],
+        },
+        info: {
+          light: colors.sky["300"],
+          dark: colors.sky["600"],
+        },
+        success: {
+          light: colors.emerald["300"],
+          dark: colors.emerald["600"],
+        },
+        warning: {
+          light: colors.amber["300"],
+          dark: colors.amber["600"],
+        },
+        error: {
+          light: colors.red["300"],
+          dark: colors.red["600"],
+        },
       },
     },
   },
