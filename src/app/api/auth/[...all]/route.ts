448dc3b5da706e1b90d6bca1dd30884c14690091
Yong Le He <ygle.he@gmail.com>
2024-11-30 23:25:05 -0500

    refactor: organize server folder

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "~/server/db/auth"; // path to your auth file
+import { auth } from "~/server/auth/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(auth);
--- src/server/auth/auth.ts
+++ src/server/auth/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
-import { db } from "../db/index";
+import { db } from "~/server/db/index";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import * as schema from "./schemas/auth";
+import * as schema from "~/server/db/schemas/auth";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
