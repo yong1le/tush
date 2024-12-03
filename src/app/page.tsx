aeb916d01d3b1667506ec34241cee25ceffd790f
Yong Le He <ygle.he@gmail.com>
2024-12-02 21:26:59 -0500

    refactor: use padding instead of margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Button } from "@headlessui/react";
 
 export default function Home() {
   return (
-    <div className="px-10">
-      <div className="mt-28 flex flex-col items-stretch gap-10 md:items-center">
-        <h1 className="text-center text-4xl font-bold">
+    <div className="px-10 pt-28">
+      <div className="flex flex-col items-stretch gap-10 md:items-center">
+        <h1 className="text-center text-4xl font-bold md:text-6xl">
           Create Perfect Laptop Mockups from Your Images
         </h1>
 
