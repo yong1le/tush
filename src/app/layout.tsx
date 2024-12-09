abfe418344d5ac2698d22de0c3e637e4de94d29b
Yong Le He <ygle.he@gmail.com>
2024-12-08 19:21:28 -0500

    chore: add tailwind classname extension

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,20 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className={`${GeistSans.variable} `}>
       <body className="dark:bg-base-dark">
-        <div className="relative h-screen bg-gradient-to-br from-transparent via-blue-400 via-30% to-transparent">
-          <div className="absolute inset-0 bg-gradient-to-br from-transparent via-fuchsia-400 via-60% to-transparent opacity-60"></div>
-          <div className="h-full bg-base-light/80 text-primary-light backdrop-blur-xl backdrop-opacity-0 dark:bg-base-dark/85 dark:text-primary-dark">
+        <div
+          className="relative h-screen bg-gradient-to-br from-transparent via-blue-400 via-30%
+            to-transparent"
+        >
+          <div
+            className="absolute inset-0 bg-gradient-to-br from-transparent via-fuchsia-400 via-60%
+              to-transparent opacity-40"
+          ></div>
+          <div
+            className="h-full bg-base-light/90 text-primary-light backdrop-blur-xl backdrop-opacity-0
+              dark:bg-base-dark/70 dark:text-primary-dark dark:md:bg-base-dark/85"
+          >
             <Header />
             <div>{children}</div>
           </div>
