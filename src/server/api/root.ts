15c2779819bfd73e95043a12e232ee0568a37995
Yong Le He <ygle.he@gmail.com>
2024-12-13 18:00:20 -0500

    fix: deployment failed bc of no routers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,30 @@
-import { createCallerFactory, createTRPCRouter } from "./trpc";
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  publicProcedure,
+} from "~/server/api/trpc";
 
-export const appRouter = createTRPCRouter({});
+/**
+ * This is the primary router for your server.
+ *
+ * All routers added in /api/routers should be manually added here.
+ */
+export const appRouter = createTRPCRouter({
+  health: publicProcedure.query(() => {
+    return {
+      health: "ok",
+    };
+  }),
+});
 
+// export type definition of API
 export type AppRouter = typeof appRouter;
 
+/**
+ * Create a server-side caller for the tRPC API.
+ * @example
+ * const trpc = createCaller(createContext);
+ * const res = await trpc.post.all();
+ *       ^? Post[]
+ */
 export const createCaller = createCallerFactory(appRouter);
