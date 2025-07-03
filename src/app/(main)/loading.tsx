61355b3eefa2b148c339e6863e5f526ccdcc5c1c
Yong Le He <ygle.he@gmail.com>
2025-07-02 20:32:41 -0400

    feat: add basic loading state

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,10 @@
+const Loading = () => {
+  return (
+    <div className="h-full flex flex-col gap-2 text-center">
+      <h1 className="text-3xl font-bold">Loading...</h1>
+      <p>Please wait while we load the content.</p>
+    </div>
+  );
+};
+
+export default Loading;
